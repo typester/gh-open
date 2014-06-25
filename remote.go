fa046e001cdd9e081cf9284f893b52d2dd40ab38
hatanakay <hatanaka.yoichi@gmail.com>
2014-06-25 10:03:52 +0900

    GH:E enabled

--- remote.go
+++ remote.go
@@ -1,58 +1,59 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"io"
-	"os"
-	"os/exec"
-	"regexp"
+    "bufio"
+    "fmt"
+    "io"
+    "os"
+    "os/exec"
+    "regexp"
+    "strings"
 )
 
 type Remote struct {
-	Name, Url, Type string
+    Name, Url, Type string
 }
 
 func DetectRemote(dir string) ([]*Remote, error) {
-	git := exec.Command("git", "remote", "-v")
-	git.Dir = dir
+    git := exec.Command("git", "remote", "-v")
+    git.Dir = dir
 
-	stdout, err := git.StdoutPipe()
-	if err != nil {
-		return nil, err
-	}
-	git.Stderr = os.Stderr
+    stdout, err := git.StdoutPipe()
+    if err != nil {
+        return nil, err
+    }
+    git.Stderr = os.Stderr
 
-	err = git.Start()
-	if err != nil {
-		return nil, err
-	}
+    err = git.Start()
+    if err != nil {
+        return nil, err
+    }
 
-	remotes := make([]*Remote, 0)
+    remotes := make([]*Remote, 0)
 
-	reader := bufio.NewReader(stdout)
-	re := regexp.MustCompile(`^(\S+)\s*(\S+)\s*\((\S+)\)`)
-	for {
-		line, err := reader.ReadString('\n')
-		if err != nil {
-			if err != io.EOF {
-				return nil, err
-			}
-			break
-		}
+    reader := bufio.NewReader(stdout)
+    re := regexp.MustCompile(`^(\S+)\s*(\S+)\s*\((\S+)\)`)
+    for {
+        line, err := reader.ReadString('\n')
+        if err != nil {
+            if err != io.EOF {
+                return nil, err
+            }
+            break
+        }
 
-		m := re.FindStringSubmatch(line)
-		if m != nil {
-			r := &Remote{m[1], m[2], m[3]}
-			remotes = append(remotes, r)
-		}
-	}
+        m := re.FindStringSubmatch(line)
+        if m != nil {
+            r := &Remote{m[1], m[2], m[3]}
+            remotes = append(remotes, r)
+        }
+    }
 
-	if err := git.Wait(); err != nil {
-		return nil, err
-	}
+    if err := git.Wait(); err != nil {
+        return nil, err
+    }
 
-	return remotes, nil
+    return remotes, nil
 }
 
 // Convert git remote url to github URL
@@ -62,31 +63,51 @@ func DetectRemote(dir string) ([]*Remote, error) {
 //   - https://github.com/username/repo.git
 //   - git://github.com/username/repo.git
 func MangleURL(url string) (string, error) {
-	ssh_re := regexp.MustCompile(`^git@(.*?):(.*?)/(.*?)\.git$`)
-	ssh2_re := regexp.MustCompile(`^ssh://git@(.*?)/(.*?)/(.*?)\.git$`)
-	https_re := regexp.MustCompile(`^https://(.*?)/(.*?)/(.*?)(?:.git)?$`)
-	git_re := regexp.MustCompile(`^git://(.*?)/(.*?)/(.*?).git$`)
+    ssh_re := regexp.MustCompile(`^git@(.*?):(.*?)/(.*?)\.git$`)
+    ssh2_re := regexp.MustCompile(`^ssh://git@(.*?)/(.*?)/(.*?)\.git$`)
+    https_re := regexp.MustCompile(`^https://(.*?)/(.*?)/(.*?)(?:.git)?$`)
+    git_re := regexp.MustCompile(`^git://(.*?)/(.*?)/(.*?).git$`)
 
-	var matches []string
+    var matches []string
 
-	if m := ssh_re.FindStringSubmatch(url); m != nil {
-		matches = m
-	} else if m := ssh2_re.FindStringSubmatch(url); m != nil {
-		matches = m
-	} else if m := https_re.FindStringSubmatch(url); m != nil {
-		matches = m
-	} else if m := git_re.FindStringSubmatch(url); m != nil {
-		matches = m
-	} else {
-		return "", fmt.Errorf("unsupported remote url: %s", url)
-	}
+    if m := ssh_re.FindStringSubmatch(url); m != nil {
+        matches = m
+    } else if m := ssh2_re.FindStringSubmatch(url); m != nil {
+        matches = m
+    } else if m := https_re.FindStringSubmatch(url); m != nil {
+        matches = m
+    } else if m := git_re.FindStringSubmatch(url); m != nil {
+        matches = m
+    } else {
+        return "", fmt.Errorf("unsupported remote url: %s", url)
+    }
 
-	return CreateURL(matches[1], matches[2], matches[3])
+    return CreateURL(matches[1], matches[2], matches[3])
 }
 
 func CreateURL(host, user, repo string) (string, error) {
-	if host != "github.com" {
-		return "", fmt.Errorf("invalid github host: %s", host)
-	}
-	return fmt.Sprintf("https://%s/%s/%s", host, user, repo), nil
+    gheHost := configGet("gh-open.ghe-host")
+    gheProtcol := configGet("gh-open.ghe-protocol")
+
+    if host != "github.com" && host != gheHost {
+        return "", fmt.Errorf("invalid github host: %s", host)
+    }
+
+    if host == gheHost {
+        if "" == gheProtcol {
+            gheProtcol = "https"
+        }
+        return fmt.Sprintf("%s://%s/%s/%s", gheProtcol, host, user, repo), nil
+    } else {
+        return fmt.Sprintf("https://%s/%s/%s", host, user, repo), nil
+    }
+}
+
+func configGet(key string) string {
+    cmd := exec.Command("git", "config", "--path", "--null", "--get", key)
+    value, err := cmd.Output()
+    if nil != err {
+        return ""
+    }
+    return strings.TrimRight(string(value), "\000")
 }
