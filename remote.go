37dc6bc4e7250579a90879607a352879d2508179
m0a <m0a@abe-mac.local>
2014-07-07 18:56:32 +0900

    add support bitbucket.org

--- remote.go
+++ remote.go
@@ -61,10 +61,13 @@ func DetectRemote(dir string) ([]*Remote, error) {
 //   - git@github.com:username/repo.git
 //   - https://github.com/username/repo.git
 //   - git://github.com/username/repo.git
+//   - git@bitbucket.org:username/repo.git //same github
+//   - https://username@bitbucket.org/username/repo.git //diffrent github
 func MangleURL(url string) (string, error) {
 	ssh_re := regexp.MustCompile(`^git@(.*?):(.*?)/(.*?)\.git$`)
 	ssh2_re := regexp.MustCompile(`^ssh://git@(.*?)/(.*?)/(.*?)\.git$`)
 	https_re := regexp.MustCompile(`^https://(.*?)/(.*?)/(.*?)(?:.git)?$`)
+	https2_re := regexp.MustCompile(`^https://.*@(.*?)/(.*?)/(.*?)(?:.git)?$`)
 	git_re := regexp.MustCompile(`^git://(.*?)/(.*?)/(.*?).git$`)
 
 	var matches []string
@@ -72,6 +75,8 @@ func MangleURL(url string) (string, error) {
 	if m := ssh_re.FindStringSubmatch(url); m != nil {
 		matches = m
 	} else if m := ssh2_re.FindStringSubmatch(url); m != nil {
+		matches = m
+	} else if m := https2_re.FindStringSubmatch(url); m != nil {
 		matches = m
 	} else if m := https_re.FindStringSubmatch(url); m != nil {
 		matches = m
@@ -85,8 +90,11 @@ func MangleURL(url string) (string, error) {
 }
 
 func CreateURL(host, user, repo string) (string, error) {
-	if host != "github.com" {
-		return "", fmt.Errorf("invalid github host: %s", host)
+	if host == "github.com" {
+		return fmt.Sprintf("https://%s/%s/%s", host, user, repo), nil
+	} else if host == "bitbucket.org" {
+		return fmt.Sprintf("https://%s/%s/%s", host, user, repo), nil
+	} else {
+		return "", fmt.Errorf("invalid github or bitbucket host: %s", host)
 	}
-	return fmt.Sprintf("https://%s/%s/%s", host, user, repo), nil
 }
--- remote_test.go
+++ remote_test.go
@@ -89,6 +89,44 @@ func TestDetectRemote(t *testing.T) {
 	}
 }
 
+//   - git@bitbucket.org:username/repo.git
+func TestDetectRemoteForBitBucket(t *testing.T) {
+	dir, err := ioutil.TempDir("", "")
+	if err != nil {
+		t.Error("failed to create tempdir:", err)
+	}
+	defer syscall.Rmdir(dir)
+
+	git := exec.Command("git", "init")
+	git.Dir = dir
+	if err := git.Run(); err != nil {
+		t.Error("failed to run git init:", err)
+	}
+
+	git = exec.Command("git", "remote", "add", "origin", "git@bitbucket.org:username/repo.git")
+	git.Dir = dir
+	if err := git.Run(); err != nil {
+		t.Error("failed to run git remote add :", err)
+	}
+
+	remotes, err := DetectRemote(dir)
+
+	if err != nil {
+		t.Error("error should be nil:", err)
+	}
+
+	if len(remotes) < 1 {
+		t.Error("unexpected remotes count")
+	}
+
+	if remotes[0].Name != "origin" {
+		t.Error("unexpected remote name", remotes[0].Name)
+	}
+	if remotes[0].Url != "git@bitbucket.org:username/repo.git" {
+		t.Error("unexpected remote url", remotes[0].Url)
+	}
+}
+
 func TestMangleURL(t *testing.T) {
 	expected := "https://github.com/username/repo"
 
@@ -124,7 +162,7 @@ func TestMangleURL(t *testing.T) {
 	if err == nil {
 		t.Error("error should be set:", err)
 	}
-	if err.Error() != "invalid github host: example.com" {
+	if err.Error() != "invalid github or bitbucket host: example.com" {
 		t.Error("unexpected error:", err)
 	}
 
@@ -136,4 +174,39 @@ func TestMangleURL(t *testing.T) {
 	if err.Error() != "unsupported remote url: git@example.com:repo.git" {
 		t.Error("unexpected error:", err)
 	}
+}
+
+// - https://username@bitbucket.org/username/repo.git
+// - git@bitbucket.org:username/repo.git
+
+func TestMangleURLforBitBucket(t *testing.T) {
+	expected := "https://bitbucket.org/username/repo"
+
+	// ssh
+	u, err := MangleURL("git@bitbucket.org:username/repo.git")
+	if err != nil {
+		t.Error("error should be nil:", err)
+	}
+	if u != expected {
+		t.Error("unexpected url:", u)
+	}
+
+	// https
+	u, err = MangleURL("https://username@bitbucket.org/username/repo.git")
+	if err != nil {
+		t.Error("error should be nil:", err)
+	}
+	if u != expected {
+		t.Error("unexpected url:", u)
+	}
+
+	// git
+	u, err = MangleURL("git@bitbucket.org:username/repo.git")
+	if err != nil {
+		t.Error("error should be nil:", err)
+	}
+	if u != expected {
+		t.Error("unexpected url:", u)
+	}
+
 }
